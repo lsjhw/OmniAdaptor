6b2ad57083c4a052e2568ab0f6650e3f55498723
hc1996 <csuhuchao@126.com>
2026-02-13 17:00:06 +0800

    restore

--- omnistream/omniop-flink-extension/java/src/main/java/com/huawei/omniruntime/flink/runtime/taskmanager/OmniTask.java
+++ omnistream/omniop-flink-extension/java/src/main/java/com/huawei/omniruntime/flink/runtime/taskmanager/OmniTask.java
@@ -570,7 +570,6 @@ public class OmniTask extends Task {
 
             // create RemoteDataFetcher for remote input channelsE
 
-            originalTaskDataFetcher = createAndStartRemoteDataFetcher(inputGates);
 
             if (!transitionState(ExecutionState.INITIALIZING, ExecutionState.RUNNING)) {
                 throw new CancelTaskException();
@@ -578,6 +577,9 @@ public class OmniTask extends Task {
             taskManagerActions.updateTaskExecutionState(new TaskExecutionState(executionId, ExecutionState.RUNNING));
             // call native restore and invoke before java
             long status = doRunRestoreNativeTask(nativeTaskRef, nativeStreamTask);
+            LOG.error("begin createAndStartRemoteDataFetcher ");
+            originalTaskDataFetcher = createAndStartRemoteDataFetcher(inputGates);
+            LOG.error("end createAndStartRemoteDataFetcher ");
             registerEventDispatcher((StreamTask<?, ?>) invokable);
             status = doRunInvokeNativeTask(nativeTaskRef, nativeStreamTask);
         } else {
